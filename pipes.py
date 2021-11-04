15c462296e1dcd1d8fba744fa31cbbdcc48723e3
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2021-11-03 21:35:35 -0400

    Add support for args and kwargs to CurryPipe

--- pipes.py
+++ pipes.py
@@ -1,17 +1,21 @@
 """Implement currying and pipes for functional tools."""
 
+from functools import reduce
+
 
 class CurryPipe:
     """Meta object capable of currying and supporting pipes."""
 
-    def __init__(self, meta_function, function):
+    def __init__(self, meta_function, function, *args, **kwargs):
         """Initialize with the meta_function and the function it will use."""
         self.meta_function = meta_function
         self.function = function
+        self.args = args
+        self.kwargs = kwargs
 
     def __call__(self, iterable):
         """Let the meta_function apply the stored function on the iterable."""
-        return self.meta_function(self.function, iterable)
+        return self.meta_function(self.function, iterable, *self.args, **self.kwargs)
 
     def __ror__(self, iterable):
         """If the object is on the rhs of a pipe call the object on the lhs iterable."""
