ce2e9a9d05b94478a2c3f30fa6f8ad3b9731e75f
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2021-11-06 08:50:14 -0400

    Refactor curried objects to use PartialBase

--- curried_map_filter_reduce.py
+++ curried_map_filter_reduce.py
@@ -1,32 +1,53 @@
 """Curried versions of map, filter, and reduce."""
 
+from abc import abstractmethod
 from functools import reduce
 
 
-class MetaCurryPipe:
-    """Adding currying and piping to meta-functions (functional tools)."""
+class PartialBase:
+    """Base class for partial transformations that are applied to an iterable."""
 
-    def __init__(self, meta_function, function, *args, **kwargs):
-        """Initialize with the meta_function and the function it will use."""
-        self.meta_function = meta_function
+    @abstractmethod
+    def transformation(self):
+        """
+        Return the transformation to be applied to iterable.
+
+        When finally applied to an iterable:
+
+            self.tranformation()(function, iterable, *args, **kwargs)
+        """
+
+    def __init__(self, function, *args, **kwargs):
+        """Initialize with the function (and args) that the transformation will use."""
         self.function = function
         self.args = args
         self.kwargs = kwargs
 
-    def __call__(self, iterable):
+    def __call__(self, iterable, *args, **kwargs):
         """Let the meta_function apply the stored function on the iterable."""
-        return self.meta_function(self.function, iterable, *self.args, **self.kwargs)
+        transformation = self.transformation()
+        updated_kwargs = {**self.kwargs, **kwargs}
+
+        return transformation(self.function, iterable, *self.args, *args, **updated_kwargs)
 
     def __ror__(self, iterable):
         """If the object is on the rhs of a pipe call the object on the lhs iterable."""
         return self(iterable)
+
 
 def emap(function, iterable=None):
     """Extended map function that supports currying and pipes."""
     if iterable:
         return map(function, iterable)
 
-    return MetaCurryPipe(map, function)
+    class PartialMap(PartialBase):
+        """PartialMap objects are curried and support pipes."""
+
+        def transformation(self):
+            """The transformation to apply to the iterable."""
+            return map
+
+    return PartialMap(function)
 
 
 def efilter(function, iterable=None):
@@ -34,7 +55,13 @@ def efilter(function, iterable=None):
     if iterable:
         return filter(function, iterable)
 
-    return MetaCurryPipe(filter, function)
+    class PartialFilter(PartialBase):
+        """PartialFilter objects are curried and support pipes."""
+
+        def transformation(self):
+            return filter
+
+    return PartialFilter(function)
 
 
 def ereduce(function, *args):
@@ -42,4 +69,10 @@ def ereduce(function, *args):
     if args and hasattr(args[0], '__iter__'):
         return reduce(function, *args)
 
-    return MetaCurryPipe(reduce, function, *args)
+    class PartialReduce(PartialBase):
+        """PartialReduce objects are curried and support pipes."""
+
+        def transformation(self):
+            return reduce
+
+    return PartialReduce(function, *args)
